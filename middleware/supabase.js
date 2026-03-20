fc47271b985e23233312adfda4b2285c6ac7f40c
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-20 17:27:44 -0600

    added supabase delete files

--- middleware/supabase.js
+++ middleware/supabase.js
@@ -6,24 +6,37 @@ const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
 );
 
-async function supabaseUpload(file_path, file) {
+async function supabaseUpload(res, file_path, file) {
 	const { data, error } = await supabase.storage
 		.from("files")
 		.upload(file_path, file);
 	if (error) {
-		throw error;
+		res.status(500).send("An error ocurred when uploading to cloud storage");
+		return;
 	}
 }
 
-async function getSupabaseDownloadUrl(file_path) {
+async function getSupabaseDownloadUrl(res, file_path) {
 	const { data, error } = await supabase.storage
 		.from("files")
 		.createSignedUrl(file_path, 60, { download: true });
 	if (error) {
-		throw error;
+		res.status(500).send("An error ocurred donwloading from cloud storage");
+		return;
 	}
 
 	return data.signedUrl;
 }
 
-export { supabaseUpload, getSupabaseDownloadUrl };
+//takes an array of file urls and deletes them
+async function supabaseDelete(res, file_paths) {
+	const { data, error } = await supabase.storage
+		.from("files")
+		.remove(file_paths);
+	if (error) {
+		res.status(500).send("An error ocurred when deleting from cloud storage");
+		return;
+	}
+}
+
+export { supabaseUpload, getSupabaseDownloadUrl, supabaseDelete };
