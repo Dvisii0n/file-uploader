834ae0d6866f0c70a7a8b9c70cca3698bd5a2a9a
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-21 18:02:14 -0600

    added auth validators

---
+++ validation/authValidation.js
@@ -0,0 +1,57 @@
+import { body } from "express-validator";
+import { prisma } from "../lib/prisma.js";
+import { lengthValidator } from "../utils/validationUtils.js";
+
+const pwLength = { min: 8, max: 255 };
+const usernameLength = { min: 3, max: 255 };
+const emailLength = { min: 8, max: 255 };
+
+const validateSignUp = [
+	body("email")
+		.trim()
+		.escape()
+		.isEmail()
+		.withMessage("Please enter a valid email")
+		.custom(async (value) => {
+			const user = await prisma.user.findFirst({ where: { email: value } });
+			if (user) {
+				throw new Error("Email is already taken");
+			}
+		}),
+
+	lengthValidator("email", emailLength),
+
+	body("username")
+		.trim()
+		.escape()
+		.isAlphanumeric()
+		.withMessage("Username must only contain letters and numbers")
+		.custom(async (value) => {
+			const user = await prisma.user.findFirst({ where: { username: value } });
+			if (user) {
+				throw new Error("Username is already taken");
+			}
+		}),
+
+	lengthValidator("username", usernameLength),
+
+	body("password")
+		.trim()
+		.escape()
+		.isAlphanumeric()
+		.withMessage("Password must only contain letters and numbers"),
+
+	lengthValidator("password", pwLength),
+];
+
+const validateLogin = [
+	body("username").trim().escape().isAlphanumeric(),
+
+	lengthValidator("username", usernameLength),
+
+	body("password").trim().escape().isAlphanumeric(),
+
+	lengthValidator("password", pwLength),
+];
+
+export { validateSignUp, validateLogin };
