872d5a1b9d35204e6cba78f06271dd6bb3be0b8d
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-23 19:47:30 -0600

    added file limit and size limit

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,16 +1,36 @@
 import multer, { MulterError } from "multer";
-import path from "path";
 
 //max files to upload
 const LIMIT = 5;
+const MAX_SIZE_MB = 6;
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage }).array("uploadedFiles", LIMIT);
+const upload = multer({
+	storage: storage,
+	limits: {
+		fileSize: MAX_SIZE_MB * 1000000, //6MB in bytes
+		files: LIMIT,
+	},
+}).array("uploadedFiles", LIMIT);
 
 async function multerUpload(req, res, next) {
 	upload(req, res, (err) => {
 		if (err instanceof MulterError) {
-			res.status(500).send("An error ocurred when uploading");
+			switch (err.code) {
+				case "LIMIT_FILE_SIZE":
+					res.send(`File must not exceed ${MAX_SIZE_MB} MB`);
+					return;
+
+				case "LIMIT_FILE_COUNT":
+					res.send(
+						`You can only upload a maximun of ${LIMIT} files at the same time`,
+					);
+					return;
+
+				default:
+					res.status(500).send("An error ocurred while uploading");
+					return;
+			}
 		} else if (err) {
 			next(err);
 		}
