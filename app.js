9e0d2bc3f101302801db3ed1fe0812823a3010f1
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-23 19:49:52 -0600

    changed cookie maxAge to 1 day instead of 7

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
 	expressSession({
 		cookie: {
-			maxAge: 7 * 24 * 60 * 60 * 1000, //ms
+			maxAge: 24 * 60 * 60 * 1000, //ms, one day
 		},
 		secret: process.env.SESSION_SECRET,
 		resave: true,
