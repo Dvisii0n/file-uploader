71be53eb4425524a212f128a246ce52d93c4428b
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-31 15:01:10 -0600

    implemented user expired shares check and cleaning

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,17 +2,29 @@ import { prisma } from "../lib/prisma.js";
 
 async function getHome(req, res, next) {
 	try {
-		const homeFolder = await prisma.folder.findFirst({
-			where: { parentFolderId: null, AND: { ownerId: req.user.id } },
-			select: {
-				folders: true,
-				files: true,
-				name: true,
-				id: true,
-			},
+		const folderData = await prisma.$transaction(async (tx) => {
+			const homeFolder = await tx.folder.findFirst({
+				where: { parentFolderId: null, AND: { ownerId: req.user.id } },
+				select: {
+					folders: true,
+					files: true,
+					name: true,
+					id: true,
+				},
+			});
+
+			const currentDate = new Date();
+			await tx.sharedFolder.deleteMany({
+				where: {
+					expiresAt: { lte: currentDate },
+					AND: { sharedFolder: { ownerId: req.user.id } },
+				},
+			});
+
+			return homeFolder;
 		});
 
-		res.render("home", { folderData: homeFolder });
+		res.render("home", { folderData: folderData });
 	} catch (err) {
 		next(err);
 	}
